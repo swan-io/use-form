73a59ca7e5fdd196b049c19db78ede000067359d
Mathieu Acthernoene <zoontek@gmail.com>
2021-03-26 18:31:26 +0100

    Re-order menu elements

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,10 +2,10 @@ import { Box, Flex, Text, VStack } from "@chakra-ui/layout";
 import * as React from "react";
 import { Route } from "wouter";
 import { Link } from "./components/Link";
-import { BasicForm } from "./forms/BasicForm";
-import { CheckboxesForm } from "./forms/CheckboxesForm";
 import { AsyncSubmissionForm } from "./forms/AsyncSubmissionForm";
 import { AsyncValidationForm } from "./forms/AsyncValidationForm";
+import { BasicForm } from "./forms/BasicForm";
+import { CheckboxesForm } from "./forms/CheckboxesForm";
 import { CreditCardForm } from "./forms/CreditCardForm";
 import { IBANForm } from "./forms/IBANForm";
 import { StrategiesForm } from "./forms/StrategiesForm";
@@ -34,9 +34,9 @@ export const App = () => (
 
       <VStack align="initial" spacing={1}>
         <Link href="/">Basic</Link>
+        <Link href="/strategies">Validation strategies</Link>
         <Link href="/async-validation">Async validation</Link>
         <Link href="/async-submission">Async submission</Link>
-        <Link href="/strategies">Validation strategies</Link>
         <Link href="/checkboxes">Checkboxes</Link>
         <Link href="/iban">IBAN</Link>
         <Link href="/credit-card">Credit card</Link>
@@ -44,9 +44,9 @@ export const App = () => (
     </Flex>
 
     <Route path="/" component={BasicForm} />
+    <Route path="/strategies" component={StrategiesForm} />
     <Route path="/async-validation" component={AsyncValidationForm} />
     <Route path="/async-submission" component={AsyncSubmissionForm} />
-    <Route path="/strategies" component={StrategiesForm} />
     <Route path="/checkboxes" component={CheckboxesForm} />
     <Route path="/iban" component={IBANForm} />
     <Route path="/credit-card" component={CreditCardForm} />

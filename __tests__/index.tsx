df8157f2d898e72dbbfb6d721a72a4a677d9a9c6
Mathieu Acthernoene <zoontek@gmail.com>
2021-03-30 13:08:17 +0200

    Write formStatus test

--- __tests__/index.tsx
+++ __tests__/index.tsx
@@ -1,18 +1,74 @@
-import { cleanup, render } from "@testing-library/react";
+import { cleanup, fireEvent, render } from "@testing-library/react";
 import * as React from "react";
 import { useForm } from "../src";
 
 afterEach(cleanup);
 
-test("default form status is untouched", async () => {
-  const Form = () => {
-    const { formStatus } = useForm({
-      foo: { initialValue: "" },
+test("formStatus evolve though time", async () => {
+  const Test = () => {
+    const { Field, formStatus, resetForm, submitForm } = useForm({
+      firstName: {
+        strategy: "onFirstSuccess",
+        initialValue: "",
+        validate: (value) => {
+          if (value.length < 3) {
+            return "Must be at least 3 characters";
+          }
+        },
+      },
     });
 
-    return <>formStatus: {formStatus}</>;
+    return (
+      <form onSubmit={(e) => e.preventDefault()}>
+        <Field name="firstName">
+          {({ ref, error, onBlur, onChange, valid, validating, value }) => (
+            <>
+              <label htmlFor="firstName">First name</label>
+
+              <input
+                ref={ref}
+                id="firstName"
+                onBlur={onBlur}
+                type="text"
+                value={value}
+                onChange={(e) => {
+                  e.preventDefault();
+                  onChange(e.target.value);
+                }}
+              />
+
+              {valid && <div>valid</div>}
+              {validating && <div>validating</div>}
+              {error && <div>error</div>}
+            </>
+          )}
+        </Field>
+
+        <div>formStatus: {formStatus}</div>
+
+        <button onClick={(e) => resetForm()}>Reset</button>
+        <button onClick={(e) => submitForm((values) => {})}>Submit</button>
+      </form>
+    );
   };
 
-  const { findByText } = render(<Form />);
+  const { findByLabelText, findByText } = render(<Test />);
+  await findByText("formStatus: untouched");
+
+  const input = await findByLabelText("First name");
+  const submitButton = await findByText("Submit");
+
+  fireEvent.input(input, {
+    target: { value: "Nicolas" },
+  });
+
+  await findByText("formStatus: editing");
+  fireEvent.click(submitButton);
+
+  const resetButton = await findByText("Reset");
+
+  await findByText("formStatus: submitted");
+  fireEvent.click(resetButton);
+
   await findByText("formStatus: untouched");
 });

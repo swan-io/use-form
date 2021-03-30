7afc99110514cb459424ff84e69acd6295f12851
Frederic Godin <frederic.godin@swan.io>
2021-03-30 14:58:10 +0200

    add validation tests

---
+++ __tests__/validation.tsx
@@ -0,0 +1,131 @@
+import { fireEvent, render } from "@testing-library/react";
+import * as React from "react";
+import { useForm } from "../src";
+import { resolveAfter } from "./utils/promises";
+
+test("input validation evolve though time", async () => {
+  const Test = () => {
+    const { Field, formStatus, resetForm, submitForm } = useForm({
+      firstName: {
+        strategy: "onFirstBlur",
+        initialValue: "",
+        validate: (value) => {
+          if (value.length < 3) {
+            return "Must be at least 3 characters";
+          }
+        },
+      },
+    });
+
+    return (
+      <form onSubmit={(e) => e.preventDefault()}>
+        <Field name="firstName">
+          {({ ref, error, onBlur, onChange, valid, validating, value }) => (
+            <>
+              <label htmlFor="firstName">First name</label>
+
+              <input
+                ref={ref}
+                type="text"
+                id="firstName"
+                value={value}
+                onBlur={onBlur}
+                onChange={(e) => {
+                  e.preventDefault();
+                  onChange(e.target.value);
+                }}
+              />
+
+              {valid && <div>valid</div>}
+              {validating && <div>validating</div>}
+              {error && <div>error</div>}
+            </>
+          )}
+        </Field>
+
+        <div>formStatus: {formStatus}</div>
+
+        <button onClick={(e) => resetForm()}>Reset</button>
+        <button onClick={(e) => submitForm((values) => {})}>Submit</button>
+      </form>
+    );
+  };
+
+  const { findByLabelText, findByText } = render(<Test />);
+
+  const input = await findByLabelText("First name");
+
+  fireEvent.focus(input)
+  fireEvent.blur(input)
+
+  await findByText("error");
+
+  fireEvent.input(input, {
+    target: { value: "Nicolas" },
+  });
+
+  await findByText("valid");
+});
+
+test("input validation evolve though time with async validation", async () => {
+  const Test = () => {
+    const { Field, formStatus, resetForm, submitForm } = useForm({
+      firstName: {
+        strategy: "onFirstBlur",
+        initialValue: "",
+        validate: (value) =>
+          resolveAfter(100, value.length < 3 ? "Must be at least 3 characters" : undefined),
+      },
+    });
+
+    return (
+      <form onSubmit={(e) => e.preventDefault()}>
+        <Field name="firstName">
+          {({ ref, error, onBlur, onChange, valid, validating, value }) => (
+            <>
+              <label htmlFor="firstName">First name</label>
+
+              <input
+                ref={ref}
+                type="text"
+                id="firstName"
+                value={value}
+                onBlur={onBlur}
+                onChange={(e) => {
+                  e.preventDefault();
+                  onChange(e.target.value);
+                }}
+              />
+
+              {valid && <div>valid</div>}
+              {validating && <div>validating</div>}
+              {error && <div>error</div>}
+            </>
+          )}
+        </Field>
+
+        <div>formStatus: {formStatus}</div>
+
+        <button onClick={(e) => resetForm()}>Reset</button>
+        <button onClick={(e) => submitForm((values) => {})}>Submit</button>
+      </form>
+    );
+  };
+
+  const { findByLabelText, findByText } = render(<Test />);
+
+  const input = await findByLabelText("First name");
+
+  fireEvent.focus(input)
+  fireEvent.blur(input)
+
+  await findByText("validating");
+  await findByText("error");
+
+  fireEvent.input(input, {
+    target: { value: "Nicolas" },
+  });
+
+  await findByText("validating");
+  await findByText("valid");
+});

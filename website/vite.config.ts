6c2e62c5db77a63a487bcfc1148a636fc9d568f1
Mathieu Acthernoene <zoontek@gmail.com>
2021-09-04 15:54:16 +0200

    Enable sourcemap

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/react-ux-form/",
+  build: { sourcemap: true },
   plugins: [reactRefresh()],
   resolve: { dedupe: ["react", "react-dom"] },
 });

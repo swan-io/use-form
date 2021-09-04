194b0f5855f5830b04d5dadf450dc208c1d6c6d8
zoontek <zoontek@gmail.com>
2021-09-04 13:42:34 +0000

    Update gh-pages to output generated at bef934b

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import { defineConfig } from "vite";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  dedupe: ["react", "react-dom"],
-  plugins: [reactRefresh()],
-});

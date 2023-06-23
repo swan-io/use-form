d226a0d47433d3facff866ecfeb830bab367bd36
Mathieu Acthernoene <zoontek@gmail.com>
2023-06-23 15:12:59 +0200

    Remove @testing-library/jest-dom

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,13 +1,8 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes
-// learn more: https://github.com/testing-library/jest-dom
-import matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
-import { afterEach, expect } from "vitest";
+import { afterEach } from "vitest";
 
 // https://testing-library.com/docs/react-testing-library/api/#cleanup
 afterEach(cleanup);
 
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#configuring-your-testing-environment
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
-
-expect.extend(matchers);
